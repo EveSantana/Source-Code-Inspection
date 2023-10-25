52d294b69c3d97e685b97e02137fb3d8357481ea
PatriciaMed <99925809+PatriciaMed@users.noreply.github.com>
2023-10-25 17:41:01 -0300

    Create PapelMoedaTest.java
    Criação de testes para PapelMoeda

---
+++ Source Code Inspection/Test/PapelMoedaTest.java
@@ -0,0 +1,42 @@
+package br.calebe.ticketmachine.core;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+public class PapelMoedaTest {
+  
+ 
+  @Test  
+    public void getValor(){  
+		try {
+			System.out.println("Começando teste de getValor");
+int gabarito=0; 
+   
+ int valorc=0; 
+int quantidadec=0; 
+ 
+ PapelMoeda papelmoeda  =new PapelMoeda( valorc ,quantidadec); 
+int resultado=papelmoeda.getValor();
+	  System.out.println("Espera-se="+ gabarito +" . Recebeu="+resultado);
+assertEquals(gabarito, resultado);
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+    }  
+ 
+  @Test  
+    public void getQuantidade(){  
+		try {
+			System.out.println("Começando teste de getQuantidade");
+int gabarito=0; 
+   
+ int valorc=0; 
+int quantidadec=0; 
+ 
+ PapelMoeda papelmoeda  =new PapelMoeda( valorc ,quantidadec); 
+int resultado=papelmoeda.getQuantidade();
+	  System.out.println("Espera-se="+ gabarito +" . Recebeu="+resultado);
+assertEquals(gabarito, resultado);
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+    }  
+}
