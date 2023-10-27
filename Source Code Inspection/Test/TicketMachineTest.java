edea208c4245f8880fffafa1e458a118c34dab62
EveSantana <87048983+EveSantana@users.noreply.github.com>
2023-10-27 00:06:27 -0300

    Update TicketMachineTest.java

--- Source Code Inspection/Test/TicketMachineTest.java
+++ Source Code Inspection/Test/TicketMachineTest.java
@@ -1,6 +1,9 @@
 package br.calebe.ticketmachine.core;
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
+import static org.junit.Assert.*;
+
+
+
 public class TicketMachineTest {
  
   @Test  
