0397fb1b191c7fbe47d9cef6b959b6b0c19be748
EveSantana <87048983+EveSantana@users.noreply.github.com>
2023-11-18 01:18:54 -0300

    Update TicketMachineTest.java

--- Codigo/src/Test/Codigo/TicketMachineTest.java
+++ Codigo/src/Test/Codigo/TicketMachineTest.java
@@ -1,5 +1,5 @@
 package Test;
-import br.calebe.ticketmachine.core.TicketMachine;
+import Codigo.src.br.calebe.ticketmachine.core.TicketMachine;
 import static org.junit.Test;
 import static org.junit.Assert.*;
 
