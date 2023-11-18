cb05df724996eee5599175ca3b931eb06fa728e8
EveSantana <87048983+EveSantana@users.noreply.github.com>
2023-11-18 02:38:23 -0300

    Update PapelMoedaTest.java

--- Codigo/src/Test/Codigo/PapelMoedaTest.java
+++ Codigo/src/Test/Codigo/PapelMoedaTest.java
@@ -1,4 +1,4 @@
-import src.br.calebe.ticketmachine.core.TicketMachine;
+import Source-Code-Inspection.Codigo.src.br.calebe.ticketmachine.core.TicketMachine;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 public class PapelMoedaTest {
